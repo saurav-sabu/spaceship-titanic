fe79903fdaf25817d52b9d9afc9e9c38c1add704
Saurav Sabu <saurav.sabu9@gmail.com>
2023-12-06 13:49:24 +0530

    All folder added except data folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To help rescue crews and retrieve the lost passengers, you are challenged to predict which passengers were transported by the anomaly using records recovered from the spaceship’s damaged computer system.',
+    author='Saurav Sabu',
+    license='',
+)
